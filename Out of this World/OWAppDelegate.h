9d6013fb260d915c59bc3646a14b1fb658b47ba2
ts <ts@tss-Mac.local>
2014-04-27 20:55:17 -0700

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by ts on 4/27/14.
+//  Copyright (c) 2014 mak Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  Out of this World
+//
+//  Created by ts on 4/27/14.
+//  Copyright (c) 2014 mak Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
